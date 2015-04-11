772eb0fa6c2a46b2a6eefcb22ffec169ab965c45
Maxime Mouchet <max@maxmouchet.com>
2015-04-11 22:31:04 +0200

    Add Student Record page parser

---
+++ src/main/java/tb/intranet/portail/parsers/StudentRecordPageParser.java
@@ -0,0 +1,85 @@
+package tb.intranet.portail.parsers;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import tb.intranet.portail.models.AcademicYear;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class StudentRecordPageParser {
+    private Document document;
+    private Elements recordsTables;
+
+    // For caching results
+    private ArrayList<AcademicYear> academicYears;
+    private ArrayList<Object> academicSemesters;
+    private ArrayList<Object> internships;
+
+    public StudentRecordPageParser(String html) {
+        document = Jsoup.parse(html);
+        recordsTables = findRecordsTables();
+    }
+
+    public ArrayList<AcademicYear> getAcademicYears() {
+        if (academicYears == null) {
+            academicYears = new ArrayList<AcademicYear>();
+        } else {
+            return academicYears;
+        }
+
+        Element resultsTable = recordsTables.get(3);
+        Elements rows = resultsTable.select("tr");
+
+        for (int i = 1; i < rows.size(); i++) {
+            Elements cols = rows.get(i).select("td");
+
+            AcademicYear academicYear = new AcademicYear();
+            academicYear.setSchoolYear(cols.get(0).text());
+            academicYear.setProgramme(cols.get(1).text());
+            academicYear.setCampus(cols.get(2).text());
+            academicYear.setGradesURL(cols.get(3).select("a").first().attr("href"));
+
+            DateFormat format = new SimpleDateFormat("dd/MM/yy", Locale.ENGLISH);
+
+            try {
+                academicYear.setStartDate(format.parse(cols.get(6).text()));
+            } catch (ParseException e) {
+                System.err.println("Unable to parse start date for academic year: " + academicYear.getSchoolYear());
+            }
+
+            try {
+                academicYear.setEndDate(format.parse(cols.get(7).text()));
+            } catch (ParseException e) {
+                System.err.println("Unable to parse end date for academic year: " + academicYear.getSchoolYear());
+            }
+
+            academicYears.add(academicYear);
+        }
+
+        return academicYears;
+    }
+
+    public ArrayList<Object> getAcademicSemesters() {
+//        Element academicSemestersTable = recordsTables.get(9);
+        throw new NotImplementedException();
+    }
+
+    public ArrayList<Object> getInternships() {
+//        Element internshipsTable      = recordsTables.get(6);
+        throw new NotImplementedException();
+    }
+
+    private Elements findRecordsTables() {
+        return document
+                .select("table#rg977")
+                .first()
+                .select("table");
+    }
+}
